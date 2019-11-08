7cd33d8feebc0ae09537de38fa57a2d0390c48eb
TheLastZombie <roesch.eric@protonmail.com>
2019-11-08 21:10:32 +0100

    Add BandcampTrackCover and GeniusNowPlaying

---
+++ BandcampTrackCover.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         BandcampTrackCover
+// @namespace    https://github.com/TheLastZombie/
+// @version      1.0.0
+// @description  Forces showing track instead of album covers on Bandcamp.
+// @author       TheLastZombie
+// @match        https://*.bandcamp.com/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+$(function () {
+
+    $("body").on("DOMSubtreeModified", ".title_link.primaryText", function() {
+        $.ajax({
+            url: $(".title_link.primaryText").attr("href"),
+            success: function (result) {
+
+                $("#tralbumArt a").attr("href", $(result).find("#tralbumArt a").attr("href"));
+                $("#tralbumArt a img").attr("src", $(result).find("#tralbumArt a img").attr("src"));
+
+            }
+        });
+    });
+
+});
