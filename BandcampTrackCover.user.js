7666f0b170294a7f555b7afa4ef072038e39ba92
TLZ <roesch.eric@protonmail.com>
2020-05-22 15:41:27 +0200

    Update BandcampTrackCover to 1.0.2

--- BandcampTrackCover.user.js
+++ BandcampTrackCover.user.js
@@ -1,18 +1,17 @@
 // ==UserScript==
 // @name         BandcampTrackCover
 // @namespace    https://github.com/TheLastZombie/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Forces showing track instead of album covers on Bandcamp.
 // @downloadURL  https://raw.github.com/TheLastZombie/userscripts/master/BandcampTrackCover.user.js
 // @author       TheLastZombie
 // @match        https://*.bandcamp.com/*
 // @grant        none
-// @require      https://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
 
 $(function () {
 
-    $("body").on("DOMSubtreeModified", ".title_link.primaryText", function() {
+    $("body").on("DOMSubtreeModified", ".play_cell", function() {
         $.ajax({
             url: $(".title_link.primaryText").attr("href"),
             success: function (result) {
