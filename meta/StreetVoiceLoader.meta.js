63962aab329368f786533d89d3d720c5c9acd083
TheLastZombie <TheLastZombie@users.noreply.github.com>
2021-07-08 19:16:12 +0000

    Apply automatic changes

---
+++ meta/StreetVoiceLoader.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           StreetVoiceLoader
+// @namespace      https://github.com/TheLastZombie/
+// @version        1.0.0
+// @description    Enables downloading of tracks (and hopefully soon albums) from StreetVoice.
+// @description:de Erlaubt das Herunterladen von Liedern (und hoffentlich bald Alben) von StreetVoice.
+// @homepageURL    https://github.com/TheLastZombie/userscripts#streetvoiceloader-
+// @supportURL     https://github.com/TheLastZombie/userscripts/issues/new?labels=StreetVoiceLoader
+// @downloadURL    https://raw.github.com/TheLastZombie/userscripts/master/user/StreetVoiceLoader.user.js
+// @updateURL      https://raw.github.com/TheLastZombie/userscripts/master/meta/StreetVoiceLoader.meta.js
+// @author         TheLastZombie
+// @match          https://streetvoice.com/*/songs/*
+// @grant          GM.xmlHttpRequest
+// @grant          GM_xmlhttpRequest
+// @require        https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @icon           https://raw.githubusercontent.com/TheLastZombie/userscripts/master/icons/StreetVoiceLoader.ico
+// @copyright      2021, TheLastZombie (https://github.com/TheLastZombie/)
+// @license        MIT; https://github.com/TheLastZombie/userscripts/blob/master/LICENSE
+// ==/UserScript==
