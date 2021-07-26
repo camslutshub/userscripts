3f0009a8b816d88c2db26ecf997261daadef05a0
TheLastZombie <TheLastZombie@users.noreply.github.com>
2021-07-26 09:30:50 +0000

    Apply automatic changes

--- meta/StreetVoiceLoader.meta.js
+++ meta/StreetVoiceLoader.meta.js
@@ -1,17 +1,20 @@
 // ==UserScript==
 // @name           StreetVoiceLoader
 // @namespace      https://github.com/TheLastZombie/
-// @version        1.1.0
-// @description    Enables downloading of tracks (and hopefully soon albums) from StreetVoice.
-// @description:de Erlaubt das Herunterladen von Liedern (und hoffentlich bald Alben) von StreetVoice.
+// @version        1.2.0
+// @description    Enables downloading of tracks and albums from StreetVoice.
+// @description:de Erlaubt das Herunterladen von Liedern und Alben von StreetVoice.
 // @homepageURL    https://github.com/TheLastZombie/userscripts#streetvoiceloader-
 // @supportURL     https://github.com/TheLastZombie/userscripts/issues/new?labels=StreetVoiceLoader
 // @downloadURL    https://raw.github.com/TheLastZombie/userscripts/master/user/StreetVoiceLoader.user.js
 // @updateURL      https://raw.github.com/TheLastZombie/userscripts/master/meta/StreetVoiceLoader.meta.js
 // @author         TheLastZombie
 // @match          https://streetvoice.com/*/songs/*
+// @connect        streetvoice.com
 // @grant          GM.xmlHttpRequest
 // @grant          GM_xmlhttpRequest
+// @require        https://cdn.jsdelivr.net/gh/gildas-lormeau/zip.js@21e1832c1ab1f8fdaffeaad0305af84040987538/dist/zip.min.js
+// @require        https://cdn.jsdelivr.net/gh/eligrey/FileSaver.js@5bb701bd6ea05a02836daf8ef88ec350a1dd4d83/dist/FileSaver.min.js
 // @require        https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @icon           https://raw.githubusercontent.com/TheLastZombie/userscripts/master/icons/StreetVoiceLoader.ico
 // @copyright      2021, TheLastZombie (https://github.com/TheLastZombie/)
