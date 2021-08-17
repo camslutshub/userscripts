c645c5f118ba6980f3921b7b86edef249e468256
TheLastZombie <TheLastZombie@users.noreply.github.com>
2021-08-17 09:08:33 +0000

    Apply automatic changes

---
+++ meta/YesPlease.meta.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name            YesPlease
+// @name:de         YesPlease
+// @name:en         YesPlease
+// @namespace       https://github.com/TheLastZombie/
+// @version         1.0.0
+// @description     A (not yet) drop-in replacement for the No, thanks. extension.
+// @description:de  (Noch k)ein Ersatz für die No, thanks.-Browsererweiterung.
+// @description:en  A (not yet) drop-in replacement for the No, thanks. extension.
+// @homepageURL     https://github.com/TheLastZombie/userscripts#yesplease-
+// @supportURL      https://github.com/TheLastZombie/userscripts/issues/new?labels=YesPlease
+// @contributionURL https://ko-fi.com/rcrsch
+// @downloadURL     https://raw.github.com/TheLastZombie/userscripts/master/user/YesPlease.user.js
+// @updateURL       https://raw.github.com/TheLastZombie/userscripts/master/meta/YesPlease.meta.js
+// @author          TheLastZombie
+// @match           *://*/*
+// @connect         no-thanks-extension.com
+// @connect         *
+// @grant           GM.getValue
+// @grant           GM_getValue
+// @grant           GM.openInTab
+// @grant           GM_openInTab
+// @grant           GM.registerMenuCommand
+// @grant           GM_registerMenuCommand
+// @grant           GM.setValue
+// @grant           GM_setValue
+// @grant           GM.xmlHttpRequest
+// @grant           GM_xmlhttpRequest
+// @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @require         https://openuserjs.org/src/libs/sizzle/GM_config.js
+// @icon            https://raw.githubusercontent.com/TheLastZombie/userscripts/master/icons/YesPlease.png
+// @copyright       2021, TheLastZombie (https://github.com/TheLastZombie/)
+// @license         MIT; https://github.com/TheLastZombie/userscripts/blob/master/LICENSE
+// ==/UserScript==
