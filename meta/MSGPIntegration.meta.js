39fa38d06fbebff0b1b43db1d5de2d0350fef502
TheLastZombie <TheLastZombie@users.noreply.github.com>
2021-08-19 07:59:11 +0000

    Apply automatic changes

---
+++ meta/MSGPIntegration.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name            MSGPIntegration
+// @name:de         MSGPIntegration
+// @name:en         MSGPIntegration
+// @namespace       https://github.com/TheLastZombie/
+// @version         1.0.0
+// @description     Allows access to the Microsoft Store Generation Project from within Microsoft Store itself.
+// @description:de  Integriert das Microsoft Store Generation Project in den Microsoft Store selbst.
+// @description:en  Allows access to the Microsoft Store Generation Project from within Microsoft Store itself.
+// @homepageURL     https://github.com/TheLastZombie/userscripts#msgpintegration-
+// @supportURL      https://github.com/TheLastZombie/userscripts/issues/new?labels=MSGPIntegration
+// @contributionURL https://ko-fi.com/rcrsch
+// @downloadURL     https://raw.github.com/TheLastZombie/userscripts/master/user/MSGPIntegration.user.js
+// @updateURL       https://raw.github.com/TheLastZombie/userscripts/master/meta/MSGPIntegration.meta.js
+// @author          TheLastZombie
+// @match           https://www.microsoft.com/*/p/*/*
+// @connect         store.rg-adguard.net
+// @grant           GM.xmlHttpRequest
+// @grant           GM_xmlhttpRequest
+// @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @icon            https://raw.githubusercontent.com/TheLastZombie/userscripts/master/icons/MSGPIntegration.ico
+// @copyright       2021, TheLastZombie (https://github.com/TheLastZombie/)
+// @license         MIT; https://github.com/TheLastZombie/userscripts/blob/master/LICENSE
+// ==/UserScript==
