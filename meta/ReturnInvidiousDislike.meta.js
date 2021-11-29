8c211e58f6741f2b6e62cad0973184f076dba050
TheLastZombie <TheLastZombie@users.noreply.github.com>
2021-11-29 07:06:12 +0000

    Apply automatic changes

---
+++ meta/ReturnInvidiousDislike.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name            ReturnInvidiousDislike
+// @name:de         ReturnInvidiousDislike
+// @name:en         ReturnInvidiousDislike
+// @namespace       https://github.com/TheLastZombie/
+// @version         1.0.0
+// @description     Displays the dislike count of videos accessed via Invidious.
+// @description:de  Zeigt die Dislike-Anzahl von Videos auf Invidious an.
+// @description:en  Displays the dislike count of videos accessed via Invidious.
+// @homepageURL     https://github.com/TheLastZombie/userscripts#returninvidiousdislike-
+// @supportURL      https://github.com/TheLastZombie/userscripts/issues/new?labels=ReturnInvidiousDislike
+// @contributionURL https://ko-fi.com/rcrsch
+// @downloadURL     https://raw.github.com/TheLastZombie/userscripts/master/user/ReturnInvidiousDislike.user.js
+// @updateURL       https://raw.github.com/TheLastZombie/userscripts/master/meta/ReturnInvidiousDislike.meta.js
+// @author          TheLastZombie
+// @match           *://*/watch?v=*
+// @connect         return-youtube-dislike-api.azurewebsites.net
+// @grant           GM.xmlHttpRequest
+// @grant           GM_xmlhttpRequest
+// @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @icon            https://raw.githubusercontent.com/TheLastZombie/userscripts/master/icons/ReturnInvidiousDislike.png
+// @copyright       2021, TheLastZombie (https://github.com/TheLastZombie/)
+// @license         MIT; https://github.com/TheLastZombie/userscripts/blob/master/LICENSE
+// ==/UserScript==
