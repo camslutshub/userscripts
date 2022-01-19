159f9e7fdbdfe411feca2c1a96364d532dbe5609
TheLastZombie <TheLastZombie@users.noreply.github.com>
2022-01-19 07:59:43 +0000

    Apply automatic changes

---
+++ meta/ViewOnYPMemoryHole.meta.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name            ViewOnYPMemoryHole
+// @name:de         ViewOnYPMemoryHole
+// @name:en         ViewOnYPMemoryHole
+// @namespace       https://github.com/TheLastZombie/
+// @version         1.0.0
+// @description     An add-on for ViewOnYP that adds support for Memory Hole.
+// @description:de  Ein Add-on für ViewOnYP, das Unterstützung für Memory Hole hinzufügt.
+// @description:en  An add-on for ViewOnYP that adds support for Memory Hole.
+// @homepageURL     https://github.com/TheLastZombie/userscripts#viewonypmemoryhole-
+// @supportURL      https://github.com/TheLastZombie/userscripts/issues/new?labels=ViewOnYPMemoryHole
+// @contributionURL https://ko-fi.com/rcrsch
+// @downloadURL     https://raw.github.com/TheLastZombie/userscripts/master/user/ViewOnYPMemoryHole.user.js
+// @updateURL       https://raw.github.com/TheLastZombie/userscripts/master/meta/ViewOnYPMemoryHole.meta.js
+// @author          TheLastZombie
+// @match           *://www.patreon.com/*
+// @connect         api.memoryhole.cc
+// @grant           GM.deleteValue
+// @grant           GM_deleteValue
+// @grant           GM.getValue
+// @grant           GM_getValue
+// @grant           GM.registerMenuCommand
+// @grant           GM_registerMenuCommand
+// @grant           GM.setValue
+// @grant           GM_setValue
+// @grant           GM.xmlHttpRequest
+// @grant           GM_xmlhttpRequest
+// @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @icon            https://raw.githubusercontent.com/TheLastZombie/userscripts/master/icons/ViewOnYPMemoryHole.png
+// @copyright       2022, TheLastZombie (https://github.com/TheLastZombie/)
+// @license         MIT; https://github.com/TheLastZombie/userscripts/blob/master/LICENSE
+// ==/UserScript==
